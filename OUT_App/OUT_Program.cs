257208378606cec8a6347f2b16eaf4d980bc9190
Alex Merqury <almgp@yandex.ru>
2026-06-09 10:35:03 -0700

    OUT route startup through Raylib app

--- OUT_App/OUT_Program.cs
+++ OUT_App/OUT_Program.cs
@@ -7,18 +7,10 @@ public static class OUT_Program
     public static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.CursorVisible = false;
-        try
-        {
-            OUT_Content content = OUT_Content.Load("OUT_Content");
-            OUT_State state = OUT_Bootstrap.NewGame(content);
-            OUT_App app = new(state);
-            app.Run();
-        }
-        finally
-        {
-            Console.ResetColor();
-            Console.CursorVisible = true;
-        }
+
+        OUT_Content content = OUT_Content.Load("OUT_Content");
+        OUT_State state = OUT_Bootstrap.NewGame(content);
+        OUT_RaylibApp app = new(state);
+        app.Run();
     }
 }
