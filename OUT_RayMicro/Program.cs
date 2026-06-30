b273223691c318204b4657ee8427c8852f396f96
Alex Merqury <almgp@yandex.ru>
2026-06-30 00:47:50 -0700

    OUT RayMicro: log managed startup crashes

--- OUT_RayMicro/Program.cs
+++ OUT_RayMicro/Program.cs
@@ -1,3 +1,14 @@
 using OUT_RayMicro.Runtime;
 
-OutmApp.Run();
+try
+{
+    OutmCrashLog.Write("Program start");
+    OutmApp.Run();
+    OutmCrashLog.Write("Program clean exit");
+}
+catch (Exception ex)
+{
+    OutmCrashLog.Write("MANAGED CRASH\n" + ex);
+    Console.Error.WriteLine(ex);
+    throw;
+}
